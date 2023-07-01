c09877456af18f0f6256fa3db96a31a5844229e8
Balam <balcaraza@gmail.com>
2023-06-30 22:40:02 -0600

    first commit

---
+++ logica.js
@@ -0,0 +1,55 @@
+const textArea = document.querySelector(".text-area"); //toma el codigo de la clase tex-area en el archivo index
+const mensaje = document.querySelector(".mostrar-mensaje"); //sera la conexion a donde se mostrara el mensaje en pantalla
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(textArea.value);
+  mensaje.value = textoEncriptado;
+  mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+  }
+  return stringEncriptada;
+}
+
+function btnDesencriptar() {
+  const textoDesencriptado = desencriptar(textArea.value);
+
+  mensaje.value = textoDesencriptado;
+  textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+  //se defninen las llaves de conversion mediante un arreglo multidimensional
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  //se convierten todas las letras a minusculas
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  //se evalua el mensaje buscando coincidendias en el string vs el primer elemento del array con el comando includes
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+      //si se encuentra coincidencia sera remplazada con el segundo elemento del array
+      stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+    }
+  }
+}
